212efa8e290405fa1871be61b8b6bd8e3e9297d1
Pravin <praviningle0@gmail.com>
2023-02-03 13:56:29 +0530

    second commit

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from shop import app
+import logging as logger
 
 
 if __name__ == '__main__':
-
+    logger.debug("Starting Flask Server")
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=True)
